0480412c484d8dd471973cbaf37be5f389b160eb
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-10-11 05:24:02 +0200

    Removed redundant fonts

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const iconStyle = { display: "inline", height: "0.8rem" };
 
 export const Footer = ({ location }: { location: string }) => {
   console.info(
-    `Made with %c♥%c in ${location}`,
+    `Developed with %c♥%c in ${location}`,
     "color: #e25555",
     "color: unset"
   );
